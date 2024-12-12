da9c1ee29f293eecde713360976dd2315e153ec0
val <valquel@pm.me>
2024-12-12 16:45:56 +0400

    Update vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/address-emoji-checksum-dapp/",
   plugins: [
     vue(),
     vueJsx(),
