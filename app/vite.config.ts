155ac9a90d5ba5a64e3a16e99ccb313dd82744d4
val <valquel@pm.me>
2024-12-12 17:32:48 +0400

    Change base in vite config to accomodate for custom domain

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/address-emoji-checksum-dapp/",
+  base: "/",
   plugins: [
     vue(),
     vueJsx(),
